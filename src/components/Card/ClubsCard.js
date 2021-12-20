3db0fd34f82477e5565614d07f92a6ce133b1000
Satria Pratama Taruna <satriapratamat7@gmail.com>
2021-12-20 11:36:49 +0700

    adding clubscard and classescard component

---
+++ src/components/Card/ClubsCard.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Card, CardContent, CardMedia, CardActionArea, Typography } from '@mui/material';
+
+
+export default function ClubsCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="160"
+          image="/public/clubs-example.png"
+          alt="Gelud Gym"
+        />
+        <CardContent>
+            <Typography variant="body2" color="text.secondary">
+                MON-SUN, 07.00 - 21.30
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+                Gelud Gym
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                Jalan Kusuma No.9 Grogol Petamburan
+            </Typography>
+            {/* <Typography className={styles.clubtime} variant="body2" color="text.secondary">
+                MON-SUN, 07.00 - 21.30
+            </Typography>
+            <Typography className={styles.clubname} gutterBottom variant="h5" component="div">
+                Gelud Gym
+            </Typography>
+            <Typography className={styles.clubloc} variant="body2" color="text.secondary">
+                Jalan Kusuma No.9 Grogol Petamburan
+            </Typography> */}
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
