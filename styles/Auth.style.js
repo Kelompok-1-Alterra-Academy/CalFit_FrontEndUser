2251fa7da3ef08d13ae22fcbbdb54153ec72bdca
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-07 22:17:20 +0700

    refactor(register-styles): remove backgroundcolor

--- styles/Auth.style.js
+++ styles/Auth.style.js
@@ -2,7 +2,6 @@ import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: "white",
     display: "flow-root",
     minHeight: "100vh",
     maxWidth: "576px",
