e80d27bc4e6a43b841bee12bd4da382f78a473b1
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-01-09 09:51:27 +0700

    chore: merge rebase conflict

--- pages/clubs/index.js
+++ pages/clubs/index.js
@@ -3,15 +3,15 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { Grid, Paper, Typography } from "@mui/material";
-import { styled } from '@mui/material/styles';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import ClubsCard from "../../src/components/Card/ClubsCard";
+import { styled } from "@mui/material/styles";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import ClubsCardGrid from "../../src/components/Card/ClubsCardGrid";
 import styles from "../../styles/clubs/Index.module.css";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
@@ -24,21 +24,26 @@ export default function Clubs() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main style={{
-        display: "flex",
-        flexDirection: "column",
-        color: "#FEFEFE",
-        minHeight: "100vh",
-        zIndex: "2",
-      }}>
-        <div style={{
+      <main
+        style={{
           display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "auto",
-          height: "65px",
-        }}>
+          flexDirection: "column",
+          color: "#FEFEFE",
+          minHeight: "100vh",
+          zIndex: "2",
+          paddingBottom: "25px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "auto",
+            height: "65px",
+          }}
+        >
           <h1>Clubs</h1>
           <Link href="/account">
             <Image
@@ -50,37 +55,25 @@ export default function Clubs() {
             />
           </Link>
         </div>
-        <div style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginTop: "20px",
-        }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginTop: "20px",
+          }}
+        >
           <h3>Explore All Clubs</h3>
-          <FilterAltIcon style={{
-            color: "#FEFEFE",
-            fontSize: "30px",
-          }} />
+          <FilterAltIcon
+            style={{
+              color: "#FEFEFE",
+              fontSize: "30px",
+            }}
+          />
         </div>
-        <Grid container spacing={2}>
-          {Array.from({ length: 10 }).map((_, i) => (
-            <Grid item xs={6} key={i}>
-              {/* <Item>xs=6</Item> */}
-              <ClubsCard />
-            </Grid>
-          ))}
-          {/* <Grid item xs={4}>
-            <Item>xs=4</Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>xs=4</Item>
-          </Grid>
-          <Grid item xs={8}>
-            <Item>xs=8</Item>
-          </Grid> */}
-        </Grid>
-      </main >
-    </div >
+        <ClubsCardGrid />
+      </main>
+    </div>
   );
 }
