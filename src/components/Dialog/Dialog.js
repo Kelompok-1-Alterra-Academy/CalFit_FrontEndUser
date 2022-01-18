b5e6e5d796ce250b7f689705e4fc993a98019fa5
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-18 17:59:53 +0700

    refactor(dialog): handling error message in alert

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -10,45 +10,70 @@ import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 import { Button } from "@mui/material";
 import { useRouter } from "next/router";
 import { bookingClass } from "../../utils/fetchApi/classes";
-
-const banks = ["BCA", "MANDIRI"];
+import { useDispatch, useSelector } from "react-redux";
+import { showAlert } from "../../store/AlertReducers";
+import { CustomAlert } from "../Alert/Alert";
 
 export default function CustomDialog({ setDialog, type, data, setLoading }) {
   const [open, setOpen] = useState(true);
   const router = useRouter();
+  const dispatch = useDispatch();
+  const alert = useSelector((state) => state.alert.alertContent);
+  const banks = ["BCA"];
 
   const handleClose = () => {
-    setOpen(false);
     setDialog(false);
   };
 
   const handleListItemClick = (id) => {
+    setDialog(false);
     data = { ...data, payment_id: id };
-    type === "payment" && bookingClass(setLoading, data);
-    setOpen(false);
-    router.push("/");
+    if (type === "payment") {
+      bookingClass(setLoading, data);
+      dispatch(
+        showAlert({
+          alertContent: {
+            message: "Booking successful",
+            status: true,
+          },
+        })
+      );
+      setTimeout(() => {
+        dispatch(
+          showAlert({
+            alertContent: {
+              status: false,
+            },
+          })
+        );
+        router.push("/account/bookings");
+      }, 2000);
+    }
   };
 
   return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Choose Payment Method</DialogTitle>
-      <List sx={{ pt: 0 }}>
-        {banks.map((bank, i) => (
-          <ListItem
-            button
-            onClick={() => handleListItemClick(i + 1)}
-            key={bank}
-          >
-            <ListItemAvatar>
-              <AccountBalanceIcon>
-                <PersonIcon />
-              </AccountBalanceIcon>
-            </ListItemAvatar>
-            <ListItemText primary={bank} />
-          </ListItem>
-        ))}
-      </List>
-      <Button></Button>
-    </Dialog>
+    <>
+      {alert.status && <CustomAlert data={alert} />}
+      <Dialog onClose={handleClose} open={open}>
+        <DialogTitle>Choose Payment Method</DialogTitle>
+        <List sx={{ pt: 0 }}>
+          {banks.map((bank, i) => (
+            <ListItem
+              button
+              onClick={() => handleListItemClick(i + 1)}
+              key={bank}
+            >
+              <ListItemAvatar>
+                <AccountBalanceIcon>
+                  <PersonIcon />
+                </AccountBalanceIcon>
+              </ListItemAvatar>
+              <ListItemText primary={bank} />
+            </ListItem>
+          ))}
+        </List>
+        <Button></Button>
+      </Dialog>
+    </>
   );
 }
