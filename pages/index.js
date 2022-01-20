bfde08d89aaa7e76617cf33f832fc18f9ef9a645
Satria Pratama Taruna <satriapratamat7@gmail.com>
2022-01-20 15:24:20 +0700

    fix conflict

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,9 @@ import ClassesCardSlides from "../src/components/card/ClassesCardSlides";
 import { useSelector } from "react-redux";
 import { CustomAlert } from "../src/components/Alert/Alert";
 import SubscriptionModal from "../src/components/Modal/SubscriptionsModal";
-// import SubscriptionModal from "../src/components/Modal/SubscriptionsModal";
 
 export default function Home() {
   const alertContent = useSelector((state) => state.alert.alertContent);
-  // const [open, setOpen] = useState(false);
-  // const handleOpen = () => setOpen(true);
-  // const handleClose = () => setOpen(false);
   return (
     <div className={styles.root}>
       <Head>
@@ -53,15 +49,6 @@ export default function Home() {
       </header>
       <main className={styles.main}>
         <div className={styles.div1}>
-          {/* <Link href="/subscription" passHref>
-            <Button className={styles.subsbutton} variant="contained">
-              Our Membership Plan
-            </Button>
-          </Link> */}
-          {/* <Button onClick={handleOpen} variant="contained">
-              Our Membership Plan
-          </Button>
-          <SubscriptionModal open={open} onClose={handleClose} /> */}
           <SubscriptionModal/>
         </div>
         <div className={styles.div2}>
