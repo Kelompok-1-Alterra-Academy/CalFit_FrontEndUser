5a460556650a4be5ec0529ca43e18694196d28cd
MuhammadFahmii <fahmiprasetio754@gmail.com>
2022-01-28 06:37:04 +0700

    refactor: change filename for semantic purpose

---
+++ styles/account/Index.styles.js
@@ -0,0 +1,69 @@
+import { makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    position: "fixed",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    display: "flex",
+    zIndex: theme.zIndex.appBar,
+    margin: "0 auto",
+    backgroundColor: theme.palette.background.dark,
+    opacity: 0.85,
+    backdropFilter: "blur(9px) !important",
+    WebkitBackdropFilter: "blur(9px) !important",
+  },
+  container: {
+    padding: "0 2rem",
+    backgroundColor: "#111d2c",
+    color: "#fefefe",
+  },
+  main: {
+    display: "flex",
+    flexDirection: "column",
+    color: "#fefefe",
+    marginTop: "50px",
+    // height: "256px",
+    fontSize: "20px",
+  },
+  userCard: {
+    display: "flex",
+    flexDirection: "row",
+    // height: "92px",
+    justifyContent: "space-between",
+    color: "#fefefe",
+    marginBottom: "20px",
+  },
+  userInfo: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    margin: "0 auto",
+  },
+  userInfoUsername: {
+    margin: "0 !important",
+    fontSize: "1.3rem !important",
+  },
+  userInfoEmail: {
+    margin: "0",
+    fontWeight: "normal",
+  },
+  userInfoMembership: {
+    margin: "0",
+  },
+  createIcon: {
+    margin: "auto !important",
+    marginRight: "0 !important",
+    color: "#fefefe",
+    cursor: "pointer",
+  },
+  menuList: {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "16px auto",
+    width: "100%",
+    cursor: "pointer",
+  },
+}));
